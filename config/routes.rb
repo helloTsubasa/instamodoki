96d2bfdd8005b94e18264f650b54b21432d9855a
helloTsubasa <tbs.kishi590@icloud.com>
2020-06-22 18:19:23 +0900

    Userモデルを作成
    - deviseをインストール
    - usersテーブルを作成
    - userモデルに空の名前を入力できない様に、バリデーションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "tweets#index" # ビューを確認する為、仮のルーティングを作成
-  resources :tweets, only: :index
+  resources :tweets, only: [:index, :new]
 end
