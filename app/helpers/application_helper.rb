966808d1749cadcb5e0487dc0d39892c2c2dafd4
helloTsubasa <tbs.kishi590@icloud.com>
2020-06-25 03:56:49 +0900

    マイページの実装

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def avatar_url(user)
+    return user.profile_pic unless user.profile_pic.nil?
+    "https://pbs.twimg.com/media/DunUuqPUYAAQtCq.jpg"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+    controllers: { registrations: 'registrations' }
   root "tweets#index" # ビューを確認する為、仮のルーティングを作成
   resources :tweets, only: [:index, :new]
+  resources :users, only: :show
 end
