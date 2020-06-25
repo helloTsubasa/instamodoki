99cf04810d62d153764bc864ad6902ab7c092516
helloTsubasa <tbs.kishi590@icloud.com>
2020-06-26 02:57:52 +0900

    プロフィール編集機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+  has_many :tweets, dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -6,4 +9,17 @@ class User < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
+
 end
