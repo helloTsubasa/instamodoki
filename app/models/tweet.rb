cbb2ca94e5e82727cf4caf27440404126ec653ad
helloTsubasa <tbs.kishi590@icloud.com>
2020-06-29 02:01:25 +0900

    いいね！機能搭載

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,9 +1,12 @@
 class Tweet < ApplicationRecord
   belongs_to :user
   has_many :photos, dependent: :destroy
-
   has_many :likes, -> { order(created_at: :desc) }, dependent: :destroy
 
   accepts_nested_attributes_for :photos
+
+  def liked_by(user)
+    Like.find_by(user_id: user.id, tweet_id: id)
+  end
 
 end
