606e780c620733bb62141538bdaa1786ae9c215b
helloTsubasa <tbs.kishi590@icloud.com>
2020-07-30 04:09:56 +0900

    create・destroyアクションを定義

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,28 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.new(comment_params)
+    @tweet = @comment.tweet
+    if @comment.save
+      respond_to :js
+    else
+      flash[:alert] = "コメントに失敗しました"
+    end
+  end
+
+  def destroy
+    @comment = Comment.find_by(id: params[:id])
+    @tweet = @comment.tweet
+    if @comment.destroy
+      respond_to :js
+    else
+      flash[:alert] = "コメントの削除に失敗しました"
+    end
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:user_id, :post_id, :comment)
+    end
+
 end
