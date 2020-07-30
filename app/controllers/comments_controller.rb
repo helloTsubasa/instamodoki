3ec5bacfe921b6ba71fb8ef59a4fd88a1493db3f
helloTsubasa <tbs.kishi590@icloud.com>
2020-07-31 01:12:35 +0900

    コメント機能追加
    viewはこれから

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -24,7 +24,7 @@ class CommentsController < ApplicationController
 
   private
     def comment_params
-      params.require(:comment).permit(:user_id, :post_id, :comment)
+      params.require(:comment).permit(:user_id, :tweet_id, :comment)
     end
 
 end
