55f77c98b9ea20ab7991f5fe577b096e387058c8
helloTsubasa <tbs.kishi590@icloud.com>
2020-08-04 04:28:56 +0900

    チャット画面のview雛形を作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root "tweets#index" # ビューを確認する為、仮のルーティングを作成
 
   resources :users, only: :show
+  resources :messages, only: :index
 
   resources :tweets, only: [:index, :new, :create, :show, :destroy] do
     resources :photos, only: :create
