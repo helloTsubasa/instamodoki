5b250291bd9570ce5c732773d39b112a29e033fd
helloTsubasa <tbs.kishi590@icloud.com>
2020-08-04 08:37:37 +0900

    アソシエーションとバリデーションの定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :comments
   has_many :group_users
   has_many :groups, through: :group_users
+  has_many :messages
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
