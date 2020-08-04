7aa8f312e88224b3ca5d5bd2db571cf747f3d0f0
helloTsubasa <tbs.kishi590@icloud.com>
2020-08-04 10:26:12 +0900

    返ってきたJSONをdoneメソッドで受取り、HTMLを形成
    1. HTMLを作成するメソッドを作成
    2.Ajaxの通信の処理が正しく行われ、値がdoneで引数として受け取れているかどうかをconsole.logで確認

--- app/assets/javascripts/messages/message.js
+++ app/assets/javascripts/messages/message.js
@@ -1,4 +1,45 @@
 $(function(){
+  function buildHTML(message){
+    if( message.image ){
+      let html =
+        `<div class="Message-box">
+          <div class="Message-box__message-info">
+            <div class="Message-box__message-info__name">
+              ${message.user_name}
+            </div>
+            <div class="Message-box__message-info__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="Message-box__message">
+            <p class="Message-box__message__content">
+              ${message.content}
+            </p>
+            <img class="Message-box__message__image" src=${message.image}>
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html =
+        `<div class="Message-box">
+          <div class="Message-box__message-info">
+            <div class="Message-box__message-info__name">
+              ${message.user_name}
+            </div>
+            <div class="Message-box__message-info__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="Message-box__message">
+            <p class="Message-box__message__content">
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+      return html;
+    };
+  }
+  
   $('.New-message').on('submit', function(e){
     e.preventDefault()
     let formData = new FormData(this)
@@ -10,6 +51,10 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data)
+      console.log(html)
     })
   })
 });
