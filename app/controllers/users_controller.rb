1c4745a65b1236277762a44cdba9e33816bc26f9
helloTsubasa <tbs.kishi590@icloud.com>
2020-08-04 10:48:56 +0900

    API側の準備とテキストフィールドの作成
    ・users#indexのルーティングを設定
    ・collection_checkboxesを削除し、テキストフィールドを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,12 @@ class UsersController < ApplicationController
   
   before_action :set_user
 
+  def index
+    respond_to do |format|
+      format.json
+    end
+  end
+  
   def show
     @tweets = Tweet.where(user_id: @user.id).order('created_at DESC')
   end
