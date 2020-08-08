f7dd2e76d775bf5d38cc0625912002a04ac03fed
helloTsubasa <tbs.kishi590@icloud.com>
2020-08-09 07:00:58 +0900

    edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, uniqueness: true
-  validates :name, presence: true, length: { minimum: 8 }
+  validates :password, presence: true, length: { minimum: 8 }
   
   def update_without_current_password(params, *options)
     params.delete(:current_password)
