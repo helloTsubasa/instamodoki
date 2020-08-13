c5315afab6344f704965f2250d85e6f42e827adc
helloTsubasa <tbs.kishi590@icloud.com>
2020-08-13 23:08:33 +0900

    UserとRelationshipの関連付け
    active_relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :group_users
   has_many :groups, through: :group_users
   has_many :messages, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
