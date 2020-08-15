c8f69fa21ca1666bbbd7594fa28a2930f7e82b2a
helloTsubasa <tbs.kishi590@icloud.com>
2020-08-16 00:55:26 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :groups, through: :group_users
   has_many :messages, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :active_relationships,class_name:  "Relationship", foreign_key: "following_id", dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "following_id", dependent: :destroy
 
 
   # Include default devise modules. Others available are:
